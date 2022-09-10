2519065737d49dbbefd42bed74bcb246a70b3724
JuanVaquiro <jvaquiro23@itfip.edu.co>
2022-09-10 18:39:12 -0500

    install React-pds renderizar PDF en medalleria

--- src/components/Medalleria.jsx
+++ src/components/Medalleria.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
 import Constante from '../constante'
-// import {  Document, StyleSheet } from '@react-pdf/renderer';
+import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer'
+import MedalleriaPDF from '../renderPDF/medalleriaPDF'
 
 const SearchMedalleria = () => {
   const [users, setUsers] = useState([])
   const [search, setSearch] = useState('')
+  const [verPDF, setVerPDF] = useState(false)
 
   const folderUser = async () => {
     // const resp = await fetch(`${Constante.RUTA_API}/obtener_medalleria.php`)
@@ -19,22 +21,40 @@ const SearchMedalleria = () => {
     console.log(value)
   }
 
+  const mostrarPDF = () => {
+    setVerPDF(true)
+  }
+
   // metodo de filtrado
   // const resultSearch = !search ? users : users.filter((dato) => dato.name.toLowerCase().includes(search.toLocaleLowerCase()))
  
   const resultSearch = users.filter((elem) => {
     return JSON.stringify(elem).toLowerCase().includes(search.toLowerCase())
   })
-
+  console.log(users);
   useEffect(() => {
     folderUser()
   }, [])
 
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <button className='btn btn-primary'>VerPDF</button>
-      <input value={search} onChange={sercher} type="text" placeholder='Serch' />
-      <table  className="tabla">
+    <div className="flex flex-col items-center justify-center">
+      <PDFDownloadLink
+        document={<MedalleriaPDF users={users} />}
+        fileName="Medalleria.pdf"
+      >
+        <button className="btn btn-primary">Descargar PDF</button>
+      </PDFDownloadLink>
+
+      <button className="btn btn-primary" onClick={() => mostrarPDF()}>
+        Ver Documento PDF
+      </button>
+      <input
+        value={search}
+        onChange={sercher}
+        type="text"
+        placeholder="Serch"
+      />
+      <table className="tabla">
         <thead>
           <tr>
             <th>Cod</th>
@@ -46,18 +66,31 @@ const SearchMedalleria = () => {
         <tbody>
           {
             resultSearch.map((data) => (
-              <tr key={data.id}>
-                <td>{data.id}</td>
-                <td>{data.name}</td>
-                <td>{data.username}</td>
-                <td>{data.email}</td>
-              </tr>
+            <tr key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.name}</td>
+              <td>{data.username}</td>
+              <td>{data.email}</td>
+            </tr>
             ))
-           }
+          }
         </tbody>
       </table>
+      {
+        verPDF ? (
+          <div className="contenedor-pdf">
+            <button className="btn btn-danger" onClick={() => setVerPDF(false)}>
+            {" "}
+            Cerrar PDF
+            </button>
+            <PDFViewer style={{ width: "100%", height: "90vh" }}>
+              <MedalleriaPDF users={users} />
+            </PDFViewer>
+          </div>
+        ) : null
+      }
     </div>
-  )
+  );
 }
 
 export default SearchMedalleria
---
+++ src/renderPDF/medalleriaPDF.jsx
@@ -0,0 +1,108 @@
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer'
+
+const styles = StyleSheet.create({
+    tabla:{
+      display: 'tabla',
+      width: '100%',
+      margin: '10px',
+      borderStyle: 'solid',
+      borderColor: 'white',
+      borderWidth: 1
+    },
+    tablaFila:{
+      margin: 'auto',
+      flexDirection: 'row'
+    },
+    tablaColumna1: {
+      width: '28%',
+      borderStyle: 'solid',
+      borderColor: 'white',
+      borderWidth: 1,
+      borderLeftWidthL: 0,
+      borderTopWidth: 0,
+      backgroundColor: 'rgb(15, 15, 49)',
+      textAlign: 'left'
+    },
+    tablaColumna2: {
+      width: '5%',
+      borderStyle: 'solid',
+      borderColor: 'white',
+      borderWidth: 1,
+      borderLeftWidthL: 0,
+      borderTopWidth: 0,
+      backgroundColor: 'rgb(15, 15, 49)',
+      textAlign: 'left'
+    },
+    tablaCeldaHeader:{
+      margin: 5,
+      fonySize: 10,
+      fontWeight: 700,
+      color: 'white',
+      paddingTop: '2px',
+      paddingBottom: '2px',
+    },
+    tablaCeldaContents:{
+      margin: 5,
+      fontSize: 12
+    },
+    anchoColumna:{
+      width: '28%',
+      borderStyle: 'solid',
+      borderColor: 'black',
+      borderWidth: 1,
+      borderLeftWidthL: 0,
+      borderTopWidth: 0,
+    },
+    anchoColumnaNumber:{
+      width: '5%',
+      borderStyle: 'solid',
+      borderColor: 'black',
+      borderWidth: 1,
+      borderLeftWidthL: 0,
+      borderTopWidth: 0,
+    }
+});
+
+const MedalleriaPDF = ({ users }) => {
+  return (
+    <Document>
+      <Page size="A4">
+        <View style={styles.tabla}>
+          <View style={styles.tablaFila}>
+            <View style={styles.tablaColumna2}>
+              <Text style={styles.tablaCeldaHeader}>id</Text>
+            </View>
+            <View style={styles.tablaColumna1}>
+              <Text style={styles.tablaCeldaHeader}>name</Text>
+            </View>
+            <View style={styles.tablaColumna1}>
+              <Text style={styles.tablaCeldaHeader}>username</Text>
+            </View>
+            <View style={styles.tablaColumna1}>
+              <Text style={styles.tablaCeldaHeader}>email</Text>
+            </View>
+          </View>
+
+          {users.map((data) => (
+            <View style={styles.tablaFila} key={data.id}>
+              <View style={styles.anchoColumnaNumber}>
+                <Text style={styles.tablaCeldaContents}>{data.id}</Text>
+              </View>
+              <View style={styles.anchoColumna}>
+                <Text style={styles.tablaCeldaContents}>{data.name}</Text>
+              </View>
+              <View style={styles.anchoColumna}>
+                <Text style={styles.tablaCeldaContents}>{data.username}</Text>
+              </View>
+              <View style={styles.anchoColumna}>
+                <Text style={styles.tablaCeldaContents}>{data.email}</Text>
+              </View>
+            </View>
+          ))}
+        </View>
+      </Page>
+    </Document>
+  );
+}
+
+export default MedalleriaPDF;
